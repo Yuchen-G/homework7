bbc9de21ecfecb6ff57ff547429bb79a7bf91b82
高雨辰 <yc@Yuchens-MacBook-Pro.local>
2020-11-28 17:05:18 +0800

    update

--- js/weather.js
+++ js/weather.js
@@ -1,23 +1,39 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
-
+    document.querySelector('#getIt').addEventListener("click", function() {
+        document.querySelector('#forecast').style.display = block;
+    })
+    
     //Set default location if one isn't provided
     let location;
-
     // Your code here.
+    if (location == null) {
+        location = "Ann Arbor"
+    }else{
+        location = document.querySelector('#location').innerHTML;
+    }
     console.log("Location is : " + location);
 
-    //set default temperature format if one isn't provided
-    let fomat;
 
+    //set default temperature format if one isn't provided
+    let format;
     // Your code here.
-    console.log("Format is " + format);
+    document.querySelector('#fahrenheit').addEventListener("click", function() {
+        format = "imperial"
+    })
+    document.querySelector('#celcius').addEventListener("click", function() {
+        format = "metric"
+    })
+    if (format !== null) {
+        format = "imperial"
+    }
+    console.log("Format is :" + format);
+
 
     //set the query  
     let query;
     // Your code here.  
-    
     console.log("Query is :" + query);
 
     //Create and set variables for each of the elements you
