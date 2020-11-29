1ffa0bdccf73e5a680af92bca9b03cee68f41acd
高雨辰 <yc@Yuchens-MacBook-Pro.local>
2020-11-29 12:14:06 +0800

    update

--- js/weather.js
+++ js/weather.js
@@ -1,39 +1,41 @@
-function gettingJSON(){
+function gettingJSON() {
     //Display the forecast
     // Your code here.
-    document.querySelector('#getIt').addEventListener("click", function() {
-        document.querySelector('#forecast').style.display = block;
-    })
-    
+
+    document.querySelector('#forecast').style.display = "block";
+
     //Set default location if one isn't provided
     let location;
     // Your code here.
-    if (location == null) {
+    location = document.querySelector('#location').value;
+    console.log("input is :" + location);
+    if (location == "") {
         location = "Ann Arbor"
-    }else{
-        location = document.querySelector('#location').innerHTML;
-    }
-    console.log("Location is : " + location);
+    } console.log("Location is :" + location);
 
 
     //set default temperature format if one isn't provided
     let format;
     // Your code here.
-    document.querySelector('#fahrenheit').addEventListener("click", function() {
-        format = "imperial"
-    })
-    document.querySelector('#celcius').addEventListener("click", function() {
-        format = "metric"
-    })
-    if (format !== null) {
-        format = "imperial"
-    }
+    function checkMeasurement() {
+        if (document.querySelector('#fahrenheit').checked == true) {
+            format = "imperial";
+        } else if (document.querySelector('#celcius').checked == true) {
+            format = "metric";
+        } else {
+            format = "imperial";
+        }
+    } 
+    checkMeasurement();
     console.log("Format is :" + format);
 
 
     //set the query  
     let query;
     // Your code here.  
+    let api = '10b1186c53a438ae9188da846e9db7ec'
+    query = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${format}&appid=${api}`
+
     console.log("Query is :" + query);
 
     //Create and set variables for each of the elements you
@@ -43,13 +45,39 @@ function gettingJSON(){
     let temp;
     let tempImg;
     // Your code here.
+    let description;
+    let iconurl;
+    let setImg;
+    // function setAttributes(attrs) {
+    //     for (var key in attrs) {
+    //         el.setAttribute(key, attrs[key]);
+    //     }
+    // }
 
 
-    $.getJSON(query,function(json){
+
+    $.getJSON(query, function (json) {
         //Use returned json to update the values of the three 
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
 
+        console.log(json);
+
+        description = json.weather[0].description;
+        tempImg = json.weather[0].icon;
+        iconurl = "http://openweathermap.org/img/w/" + tempImg + ".png";
+        setImg = document.querySelector("#tempImg");
+        setImg.setAttribute("src", iconurl);
+        setImg.setAttribute("alt", description);
+        setImg.setAttribute("title", description);
+
+        loc = document.querySelector("#loc").innerHTML = json.name;
+        temp = document.querySelector("#temp").innerHTML = json.main.temp + " with " + description;
+
+        console.log("location is " + loc);
+        console.log("temp is " + temp);
+        console.log("weather is " + description);
+        console.log("tempImg is " + tempImg);
     });
 }
